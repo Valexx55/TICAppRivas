ad65345a3ce9fc51c5631ec39537f1dc3543d727
formacion <formacion@FCPU0113>
2018-07-11 12:29:53 +0200

    MainArrayList 1

--- src/edu/idel/val/colecciones/MainArrayList.java
+++ src/edu/idel/val/colecciones/MainArrayList.java
@@ -1,6 +1,12 @@
 package edu.idel.val.colecciones;
 
+import java.util.ArrayList;
+
 public class MainArrayList {
+	
+	public MainArrayList() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	/**
 	 * Haced un método que reciba 2 parámetros
@@ -11,5 +17,52 @@ public class MainArrayList {
 	 * el ArrayList
 	 * false --> Si el String NO está en la lista
 	 */
+	
+	public static boolean palabraEnLista
+	(ArrayList<String> lista_palabras,
+			String palabra)
+	{
+		boolean encontrada = false;
+		int indice = 0; //para recorrer
+		String palabra_actual = null;
+		while ((!encontrada)&&(indice<lista_palabras.size()))
+		{
+			palabra_actual = lista_palabras.get(indice);
+			if (palabra_actual.equals(palabra))
+			{
+				encontrada = true;
+			} else 
+			{ //voy a la siguiente
+				indice++;
+			}
+		}
+		
+		return encontrada;
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		ArrayList<String> lista = new ArrayList<String>();
+		lista.add("Laura");
+		lista.add("Andrés");
+		lista.add("Metro");
+		
+		String palabra_buscada = "Laura";
+		
+		boolean esta =palabraEnLista(lista, palabra_buscada);
+		if (esta)//esta == true
+		{
+			System.out.println("La palabra está en la lista");
+		} else
+		{
+			System.out.println("La palabra No está");
+		}
+		
+		
+		
+	}
 
 }
