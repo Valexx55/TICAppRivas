83369179c94175990ca897c6d96818c83ff9361b
formacion <formacion@FCPU0113>
2018-07-12 11:58:41 +0200

    Perro equal 1

--- src/edu/idel/val/colecciones/MainArrayList.java
+++ src/edu/idel/val/colecciones/MainArrayList.java
@@ -8,6 +8,31 @@ public class MainArrayList {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public static boolean perroEnLista 
+	(ArrayList<Perro> lista_perros, 
+			Perro perro_buscado)
+	{
+		boolean encontrado = false;
+		int indice = 0; //para recorrer
+		Perro perro_actual = null;
+		while ((!encontrado)&&(indice<lista_perros.size()))
+		{
+			perro_actual = lista_perros.get(indice);
+			if (perro_actual.equals(perro_buscado))
+			{
+				encontrado = true;
+			} else 
+			{ //voy a la siguiente
+				indice++;
+			}
+		}
+		
+		return encontrado;
+		
+	
+		
+	}
+	
 	/**
 	 * Haced un método que reciba 2 parámetros
 	 * 1 --> Un ArrayList de String
@@ -45,21 +70,41 @@ public class MainArrayList {
 	
 	
 	public static void main(String[] args) {
-		ArrayList<String> lista = new ArrayList<String>();
-		lista.add("Laura");
-		lista.add("Andrés");
-		lista.add("Metro");
 		
-		String palabra_buscada = "Laura";
+		ArrayList<Perro> lista_perros = new ArrayList<Perro>();
+		Perro perro_aran = new Perro("Galgo", 3, "Isi");
+		Perro perro_aurora = new Perro("Pastor Alemán", 0, "Simba");
+		Perro perro_nacho = new Perro("Cruce", 4, "Roko");
+		lista_perros.add(perro_aurora);
+		lista_perros.add(perro_nacho);
+		lista_perros.add(perro_aran);
 		
-		boolean esta =palabraEnLista(lista, palabra_buscada);
-		if (esta)//esta == true
+		Perro perro_buscado = new Perro("Galgo", 3, "Isi");
+		boolean esta = perroEnLista(lista_perros, perro_buscado);
+		if (esta)
 		{
-			System.out.println("La palabra está en la lista");
-		} else
+			System.out.println("El can están");
+		} 
+		else 
 		{
-			System.out.println("La palabra No está");
+			System.out.println("El can NO está");
 		}
+		
+//		ArrayList<String> lista = new ArrayList<String>();
+//		lista.add("Laura");
+//		lista.add("Andrés");
+//		lista.add("Metro");
+//		
+//		String palabra_buscada = "Laura";
+//		
+//		boolean esta =palabraEnLista(lista, palabra_buscada);
+//		if (esta)//esta == true
+//		{
+//			System.out.println("La palabra está en la lista");
+//		} else
+//		{
+//			System.out.println("La palabra No está");
+//		}
 		
 		
 		
---
+++ src/edu/idel/val/colecciones/Perro.java
@@ -0,0 +1,52 @@
+package edu.idel.val.colecciones;
+
+public class Perro {
+	
+	private String raza;
+	private int edad;
+	private String nombre;
+		
+	
+	@Override
+	public boolean equals(Object obj) {
+		boolean son_iguales = false;
+	//this es un perro
+		//y obj es el otro
+		Perro p = (Perro)obj;//hago el casting
+		
+		if (this.nombre.equals(p.nombre))
+		{
+			son_iguales = true;
+		}
+		
+		return son_iguales;
+	}
+	
+	public Perro(String raza, int edad, String nombre) {
+		super();
+		this.raza = raza;
+		this.edad = edad;
+		this.nombre = nombre;
+	}
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
